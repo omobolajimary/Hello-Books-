a52652b401c4cc43fa2a48d26dd56fc415fb36d6
omobolajimary <omobolajimary@gmail.com>
2017-10-18 17:39:14 +0100

    Add background image

--- template/js/book.js
+++ template/js/book.js
@@ -1,4 +1,45 @@
 add = () =>{
     var input= document.getElementById ("addReview").value;
+    if (input==""){
+        alert("Please review can not be empty");
+        return false;
+    }
     
+}
+validateForm = () =>{
+    var username = document.forms["myForm"]["username"].value;
+    if (usename == "") {
+        alert("Username must be filled out");
+        return false;
+    }
+    var password = document.myForm.password.value;
+    if (password == "") {
+        window.alert("Password must be filled out");
+        return false;
+    }
+    var retype_password = document.myForm.retype_password.value;
+    if (retype_password != password) {
+        window.alert("Password must Tally");
+        return false;
+    }
+    var email=document.myForm.email.value;
+    if (email == ""){
+        window.alert("Please enter your e-mail address.");
+        email.focus();
+        return false;
+    }
+
+    if (email.indexOf("@", 0) < 0){
+    window.alert("Please enter a valid e-mail address.");
+    email.focus();
+    return false;
+    }
+
+    if (email.indexOf(".", 0) < 0){
+    window.alert("Please enter a valid e-mail address.");
+    email.focus();
+    return false;
+    }
+
+return true;
 }
