17103abb486f9be9c2891b5151f3668167ccc8b9
omobolajimary <omobolajimary@gmail.com>
2017-11-13 14:41:06 +0100

    add test

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const book_controller = require('../controller/bookController');
 const user_controller = require('../controller/userController');
 
-router.post('/api/books', book_controller.create);
-router.get('/api/books', book_controller.getAllBooks);
-router.put('/api/books/:bookId', book_controller.update);
-router.post('/api/user/:userId/borrow/:bookId', user_controller.borrowBook);
-router.post('/api/user/:userId/return/:bookId', user_controller.returnBook);
-router.post('/api/user/:userId/review/:bookId', user_controller.reviewBook);
-router.post('/api/user/:userId/fav/:bookId', user_controller.markAsFavorites);
-router.get('/api/user/:userId/favbooks', user_controller.getFavoritesBook);
-router.get('/api/books/sort',user_controller.getUpvote);
+router.post('/api/v1/books', book_controller.create);
+router.get('/api/v1/books', book_controller.getAllBooks);
+router.put('/api/v1/books/:bookId', book_controller.update);
+router.post('/api/v1/user/:userId/borrow/:bookId', user_controller.borrowBook);
+router.post('/api/v1/user/:userId/return/:bookId', user_controller.returnBook);
+router.post('/api/v1/user/:userId/review/:bookId', user_controller.reviewBook);
+router.post('/api/v1/user/:userId/fav/:bookId', user_controller.markAsFavorites);
+router.get('/api/v1/user/:userId/favbooks', user_controller.getFavoritesBook);
+router.get('/api/v1/books/sort',user_controller.getUpvote);
 
 
 
